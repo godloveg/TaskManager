43303c7c14fbdca9a8d5f888b040ad87f3de3f32
CHAMWILE <rasuleyman611@gmail.com>
2025-09-01 10:41:18 +0300

    AAAAAABBBBBBBBBBBBBAAABBAAAABBB

---
+++ taskmanager/taskmanager/urls.py
@@ -0,0 +1,40 @@
+"""
+URL configuration for taskmanager project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from tasks.views import register
+from tasks import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+      # Auth (login, logout, password reset, etc.)
+    path('accounts/', include('django.contrib.auth.urls')),
+
+    # Registration
+    path('register/', register, name='register'),
+
+    # Tasks app
+    path('', include('tasks.urls')),
+    path('', views.TaskList.as_view(), name='task_list'),
+    path('task/<int:pk>/', views.TaskDetail.as_view(), name='task_detail'),
+    path('task/create/', views.TaskCreate.as_view(), name='task_create'),
+    path('task/<int:pk>/update/', views.TaskUpdate.as_view(), name='task_update'),
+    path('task/<int:pk>/delete/', views.TaskDelete.as_view(), name='task_delete'),
+
+]
+
